1d3d28640a6c3acef487d3289ead722d7ed8229c
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2017-04-20 13:34:42 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,166 @@
+var pdf = require('html-pdf')
+var html = ` <html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+     <title>
+         <!-- subject -->
+         Store Opening Oberhausen Confirmation
+         <!-- /subject -->
+     </title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!--[if !mso]><!-- -->
+     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:400,700,900">
+     <!--<![endif]-->
+     <!--[if mso]><!-- -->
+     <style type=”text/css”>
+     @import url('http://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:400,700,900');
+     </style>
+     <!--<![endif]-->
+ </head>
+ <body style="margin: 0;padding: 0;font-family: Roboto, sans-serif;width: 100%!important;">
+     <table border="0" style="margin-top:5px;border: none; border-style: collapse;border-collapse: collapse;" cellpadding="0" cellspacing="0" width="100%">
+         <tr>
+             <td>
+                 <table class="content" align="center" bordercolor="#fff" border="0" cellpadding="0" cellspacing="0" width="600" style="width: 600px;">
+                     <tr>
+                         <td colspan="3" align="center" bgcolor="#fff" style="padding-bottom: 15px;">
+                             <!-- logo --><img id="logoImg" data-logo src="" data-logo-end style="width: auto; height: auto; max-width: 145px;" width="145">
+                             <!-- /logo -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="3" align="center" bgcolor="#fff" style="padding-bottom: 15px;">
+                             <!-- banner --><img id="bannerImg" data-banner src="https://zaecheldevus.s3.amazonaws.com/777a9323-cac0-4809-9d71-09e46b2faa13.jpg" data-banner-end  style="width: auto; height: auto; max-width: 470px;" width="470">
+                             <!-- /banner -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="3" align="center" style="padding-top: 15px;font-size: 18px;font-weight: 800;">
+                             <!-- eventname -->
+                             Store Opening Oberhausen
+                             <!-- /eventname -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="3" align="center" style="padding-top: 5px; padding-bottom: 25px; font-size: 12px;">
+                             <!-- date -->
+                             June 15,2017
+                             <!-- /date -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0; color: #BEBEBE;min-width: 200px;max-width: 200px;width: 200px;">
+                             <!-- entryMSG -->
+                             Einlass
+                             <!-- /entryMSG -->
+                         </td>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0; color: #BEBEBE;min-width: 200px;max-width: 200px;width: 200px;">
+                             <!-- startMSG -->
+                             Start
+                             <!-- /startMSG -->
+                         </td>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0; color: #BEBEBE;min-width: 200px;max-width: 200px;width: 200px;">
+                             <!-- endMSG -->
+                             Ende
+                             <!-- /endMSG -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0; color: #BEBEBE;min-width: 200px;max-width: 200px;width: 200px;">
+                             <!-- entry -->
+                              6:00:00 PM
+                             <!-- /entry -->
+                         </td>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0; color: #BEBEBE;min-width: 200px;max-width: 200px;width: 200px;">
+                             <!-- start -->
+                              7:00:00 PM
+                             <!-- /start -->
+                         </td>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0; color: #BEBEBE;min-width: 200px;max-width: 200px;width: 200px;">
+                             <!-- end -->
+                              11:00:00 PM
+                             <!-- /end -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="3" align="center" style="padding: 15px;padding-top:30px;padding-bottom:0;font-size: 12px;color: #000;line-height: 22px;font-weight: 400;">
+                             <!-- title -->
+                             Vielen Dank für die Anmeldung zur Veranstaltung
+                             <!-- /title -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="3" align="center" style="padding-top: 5px;font-size: 18px;font-weight: 800;">
+                             <!-- name -->
+                             ahmar siddiqui
+                             <!-- /name -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="3" align="center" style="padding: 15px;padding-top:30px;padding-bottom:0;font-size: 12px;color: #BEBEBE;line-height: 22px;font-weight: 400;">
+                             <!-- instruction -->
+                             Bitte scannen Sie diesen QR-Code für den Zugang
+                             <!-- /instruction -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="3" align="center" bgcolor="#fff" style="height: 150px;padding-bottom: 5px;padding-top:30px">
+                             <!-- qr -->
+                             <img data-qr src="http://api-bwipjs.rhcloud.com/?bcid=qrcode&text=58f8494daba29f2a0d901cc7" data-qr-end alt="QR-code" height="150" style="width: auto;">
+                             <!-- /qr -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="3" align="center" bgcolor="#fff" style="height: 35px;padding-bottom: 25px;">
+                             <!-- barcode -->
+                             <img data-barcode src="http://api-bwipjs.rhcloud.com/?bcid=code128&text=58f8494daba29f2a0d901cc7" data-barcode-end alt="Barcode" height="35" style="width: auto;">
+                             <!-- /barcode -->
+                         </td>
+                     </tr>
+                 </table>
+                 <table class="content" align="center" bordercolor="#fff" border="0" cellpadding="0" cellspacing="0" width="600" style="width: 600px;">
+                     <tr>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0; color: #5ab8ac;width: 50%;">
+                             <!-- venueheading -->
+                             Adresse
+                             <!-- /venueheading -->
+                         </td>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0; color: #5ab8ac;width: 50%;">
+                             <!-- groupheading -->
+                             Gruppe
+                             <!-- /groupheading -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0;width: 50%;">
+                             <!-- venue -->
+                             Vapiano Oberhausen<br>Restaurant<br>Testweg 1<br>50050<br>Oberhausen
+                             <!-- /venue -->
+                         </td>
+                         <td align="center" style="padding: 5px;font-size: 12px;padding-top: 0;width: 50%;">
+                             <!-- group -->
+
+                             <!-- /group -->
+                         </td>
+                     </tr>
+                     <tr>
+                         <td colspan="2" style="padding: 5px;font-size: 12px;padding-top: 30px;width: 50%;">
+                             <!-- attach -->
+
+                             <!-- /attach -->
+                         </td>
+                     </tr>
+                 </table>
+             </td>
+         </tr>
+     </table>
+ </body>
+ </html><style>html {zoom: 0.8;} #logoImg{margin-top:50px;max-height:100px!important} #bannerImg{max-height:250px!important} img[data-qr]{max-height:80px!important}</style>`
+
+
+pdf.create(html, {
+    "format": "A4"
+}).toBuffer(function(err, buffer) {
+	console.log(err)
+	console.log('created pdf')
+})
